444778be04b1fed35ebf0540b9623d411091f074
Kaleo K <kaleo.kronberg@gmail.com>
2022-03-19 22:49:41 -0700

    Add methods for computer AI to verify a guess

--- computer.rb
+++ computer.rb
@@ -11,42 +11,46 @@ class Computer
     @computer_code
   end
 
-  # Define a function verify_code that checks each digit of a given code against another code, and returns the quality
-
-  # def verify_code(array)
-  #   computer_code = [1, 2, 3, 3]
-  #   computer_feedback = { perfect: 0, included: 0 }
-  #   array.each_with_index do |digit, position|
-  #     if computer_code[position] == digit
-  #       computer_feedback[:perfect] += 1
-  #       computer_code[computer_code.index(digit)] = nil
-  #     elsif computer_code.include?(digit)
-  #       computer_feedback[:included] += 1
-  #       computer_code[computer_code.index(digit)] = nil
-  #     end
-  #   end
-  #   computer_feedback
-  # end
-
   def verify_code(guess)
-    computer_code = @computer_code.dup
+    computer_code = [1, 5, 4, 1]
     computer_feedback = { perfect: 0, imperfect: 0 }
-    guess.each_with_index do |digit, pos|
-      if computer_code[pos] == digit
-        computer_feedback[:perfect] += 1
-        computer_code[pos] = 0
-      end
-    end
-    guess.each do |digit|
-      if computer_code.include?(digit)
-        computer_feedback[:imperfect] += 1
-        computer_code[computer_code.index(digit)] = 0
-      end
-    end
+    computer_feedback[:perfect] = check_perfects(computer_code, guess)
+    computer_feedback[:imperfect] = check_imperfects(computer_code, guess)
     computer_feedback
+  end
+
+  def check_perfects(code, guess)
+    perfects = 0
+    guess.each_with_index do |digit, pos|
+      if code[pos] == digit
+        perfects += 1
+        code[pos] = 0
+      end
+    end
+    perfects
+  end
+
+  def check_imperfects(code, guess)
+    imperfects = 0
+    guess.each_with_index do |digit, pos|
+      if code.include?(digit)
+        next if code[pos].zero?
+
+        imperfects += 1
+        code[code.index(digit)] = -1
+      end
+    end
+    imperfects
   end
 end
 
 test = Computer.new
 test.make_code
-p test.verify_code([1, 1, 2, 2])
+
+# checked against code: [1, 5, 4, 1]
+code = [1, 5, 4, 1]
+puts test.verify_code([1, 5, 4, 3]) == { perfect: 3, imperfect: 0 }
+puts test.verify_code([1, 5, 5, 4]) == { perfect: 2, imperfect: 1 }
+puts test.verify_code([5, 1, 1, 4]) == { perfect: 0, imperfect: 4 }
+puts test.verify_code([1, 5, 4, 1]) == { perfect: 4, imperfect: 0 }
+puts test.verify_code([5, 4, 5, 4]) == { perfect: 0, imperfect: 2 }
