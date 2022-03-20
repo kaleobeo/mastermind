6ce7f26fd613b7d500eb40503c8079e2cd4f92a0
Kaleo K <kaleo.kronberg@gmail.com>
2022-03-20 15:56:30 -0700

    Add tests and refactor computer verification

--- computer.rb
+++ computer.rb
@@ -2,7 +2,7 @@
 
 # Computer class that manages creating a code and verifying guesses from the player
 class Computer
-
+  attr_reader :computer_code
   # Define a function generate_code that makes a 4 digit code such as 1111, 1112, 1122, etc
 
   def make_code
@@ -11,15 +11,15 @@ class Computer
     @computer_code
   end
 
-  def verify_code(guess)
-    computer_code = [1, 5, 4, 1]
+  def verify_code(guess, solution = @computer_code)
+    code = solution.dup
     computer_feedback = { perfect: 0, imperfect: 0 }
-    computer_feedback[:perfect] = check_perfects(computer_code, guess)
-    computer_feedback[:imperfect] = check_imperfects(computer_code, guess)
+    computer_feedback[:perfect] = check_perfects(code, guess)
+    computer_feedback[:imperfect] = check_imperfects(code, guess)
     computer_feedback
   end
 
-  def check_perfects(code, guess)
+  def check_perfects(guess, code)
     perfects = 0
     guess.each_with_index do |digit, pos|
       if code[pos] == digit
@@ -30,7 +30,7 @@ class Computer
     perfects
   end
 
-  def check_imperfects(code, guess)
+  def check_imperfects(guess, code)
     imperfects = 0
     guess.each_with_index do |digit, pos|
       if code.include?(digit)
@@ -44,13 +44,19 @@ class Computer
   end
 end
 
-test = Computer.new
-test.make_code
+
+
+
+#test = Computer.new
+#test.make_code
+#p test.computer_code
+#p test.computer_code
+#puts test.verify_code([1, 1, 2, 2])
 
 # checked against code: [1, 5, 4, 1]
-code = [1, 5, 4, 1]
-puts test.verify_code([1, 5, 4, 3]) == { perfect: 3, imperfect: 0 }
-puts test.verify_code([1, 5, 5, 4]) == { perfect: 2, imperfect: 1 }
-puts test.verify_code([5, 1, 1, 4]) == { perfect: 0, imperfect: 4 }
-puts test.verify_code([1, 5, 4, 1]) == { perfect: 4, imperfect: 0 }
-puts test.verify_code([5, 4, 5, 4]) == { perfect: 0, imperfect: 2 }
+#code = [1, 5, 4, 1]
+#puts test.verify_code([1, 5, 4, 3], code) == { perfect: 3, imperfect: 0 }
+#puts test.verify_code([1, 5, 5, 4], code) == { perfect: 2, imperfect: 1 }
+#puts test.verify_code([5, 1, 1, 4], code) == { perfect: 0, imperfect: 4 }
+#puts test.verify_code([1, 5, 4, 1], code) == { perfect: 4, imperfect: 0 }
+#puts test.verify_code([5, 4, 5, 4], code) == { perfect: 0, imperfect: 2 }
--- player.rb
+++ player.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+require_relative 'display'
 # Manages input from player
 class Player
+  include Display
   # Define method get_code which prompts for a code from the player and returns it
-  def get_code
-
+  def make_code
+    player_select_code
   end
 end
